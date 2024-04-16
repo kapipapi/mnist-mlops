2d9aeeb14f39ad1cc7cc25cafb0ae1c1bf11fb67
Kacper Ledwosiński <kl@airotec.pl>
2024-04-16 16:28:25 +0200

    improve ui experience

--- app_encapsulated.py
+++ app_encapsulated.py
@@ -28,12 +28,34 @@ class DigitRecognizerApp:
     def draw_lines(self, event):
         x = event.x
         y = event.y
-        self.canvas.create_oval(x-10, y-10, x+10, y+10, fill="black")
-        self.draw.ellipse((x-10, y-10, x+10, y+10), fill=0)
+
+        size = 20
+        self.canvas.create_oval(x-size//2, y-size//2, x+size//2, y+size//2, fill="black")
+        self.draw.ellipse((x-size//2, y-size//2, x+size//2, y+size//2), fill=0)
 
     def predict_value(self):
         grayscale = self.image.convert('L')
-        resized = grayscale.resize((28,28))
+        img_data = np.array(grayscale)
+
+        # Find the rows and columns where the image is not white
+        non_white_pixels = np.where(img_data != 255)
+        min_y, max_y = np.min(non_white_pixels[0]), np.max(non_white_pixels[0])
+        min_x, max_x = np.min(non_white_pixels[1]), np.max(non_white_pixels[1])
+
+        img_data = img_data[min_y:max_y, min_x:max_x]
+
+        # Add padding to the image
+        side = max(img_data.shape[0], img_data.shape[1])
+        pad_x = max(side - img_data.shape[0], 0)
+        pad_y = max(side - img_data.shape[1], 0)
+        
+        img_data = np.pad(img_data, ((pad_x//2, pad_x//2), (pad_y//2, pad_y//2)), 'constant', constant_values=255)
+
+        from PIL import Image
+        im = Image.fromarray(img_data)
+        im.save("test.png")
+
+        resized = np.resize(img_data, (28, 28))
         transformed = np.array(resized, dtype=np.float32) / 255.
         reshaped = np.reshape(transformed, (1, 28, 28, 1))
         result = self.loaded_model.predict(reshaped)
@@ -49,7 +71,7 @@ class DigitRecognizerApp:
 
 if __name__ == "__main__":
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
-    loaded_model = mlflow.pyfunc.load_model('runs:/c2ee72bd1e9b442a87bae86e74aade20/model')
+    loaded_model = mlflow.pyfunc.load_model('runs:/cd39b8ed1b3a4f579b712181fa3576f5/mnist-model')
 
     window = tk.Tk()
     app = DigitRecognizerApp(window, loaded_model)
